fc8956a15e8745796f40916daba4cb9a8c9ab914
Ja-wa-D <jawadmohammed79@gmail.com>
2025-04-23 00:21:23 +0530

    renamed file
    renamed pricing.js to news.js

--- frontend/src/landing_page/home/Homepage.js
+++ frontend/src/landing_page/home/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import Awards from './Awards'
 import Stats from './Stats';
-import Pricing from './Pricing';
+import News from './News.';
 import Education from './Education';
 import OpenAccount from '../OpenAccount';
 import NavBar from '../NavBar';
@@ -12,20 +12,14 @@ import Footer from '../Footer';
 
 function HomePage() {
     return (  
-    
         <>
-        
-        
-        <Hero />
-        <Awards />
-        <Stats />
-        <Pricing />
-        <Education />   
-        <OpenAccount />
-        
-
+            <Hero />
+            <Awards />
+            <Stats />
+            <News/>
+            <Education />   
+            <OpenAccount />
         </>
-
     );
 }
 
