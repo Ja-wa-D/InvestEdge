1455899a4e308e25b61d2c307e9ab38d466a1f02
Ja-wa-D <jawadmohammed79@gmail.com>
2025-05-12 22:48:20 +0530

    linked to Live News

--- frontend/src/landing_page/products/RightSectionCopy.js
+++ frontend/src/landing_page/products/RightSectionCopy.js
@@ -11,7 +11,8 @@ function RightSection() {
                 <h3 className='mx-1 mt-5'>Your Personal Stock Newsroom</h3>
 
                 <p className='mt-3 text-secondary mb-2 mx-1' style={{textAlign:"left"}}> Our system scans top financial sources, social feeds,<br/> and global headlines, delivering breaking<br/> updates to help you stay    ahead of the curve. </p>
-                <a href=' 'className="mx-1 ml-5"style={{textDecoration:"None"}}>Start Your News Feed <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+                {/* <a href=' 'className="mx-1 ml-5"style={{textDecoration:"None"}}>Start Your News Feed <i class="fa fa-angle-double-right" aria-hidden="true"></i></a> */}
+                <Link to='/news'className="mx-1 ml-5"style={{textDecoration:"None"}}>Start Your News Feed <i class="fa fa-angle-double-right" aria-hidden="true"></i></Link>
                 </div>
             <div className='col-6 p-4 '>
                     <img className='mx-5 mb-5' src='media/images/signup.png' alt='news' style={{width:"70%"}} />
